8d5a3c3a3d552a1bc66d0891363cb085fcd2f0fe
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-02-19 13:08:17 -0300

    modernize parte 1

--- modernize/src/router/index.ts
+++ modernize/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainRoutes from './MainRoutes';
 import AuthRoutes from './AuthRoutes';
 
-export default createRouter({
+export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
