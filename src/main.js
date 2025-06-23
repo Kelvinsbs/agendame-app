f15d1eb22b0665f2ae0e7475eadf3f8933e855ea
Kelvinsbs <nivlek.barcelos@gmail.com>
2025-06-23 18:56:28 -0300

    pinia instalado e referenciado

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import App from './App.vue'
 import router from './router'
 import { createApp } from 'vue'
 import vuetify from './plugins/vuetify';
-import '@/scss/style.scss';
+import '@/scss/style.scss'; // @ é um alias para src
 import './plugins/yup';
 import './plugins/axios';
+import pinia from './plugins/pinia';
 
 const app = createApp(App)
 app
   .use(router)
+  .use(pinia)
   .use(vuetify)
   .mount('#app')
